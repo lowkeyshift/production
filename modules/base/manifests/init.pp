a671aedd715158100b041a51538ce71a37cc9e79
Edwin Carrasquillo <ecarra7979@gmail.com>
2017-09-17 17:55:12 -0400

    Puppet-lint spacing

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -12,34 +12,34 @@ class base {
   package { 'nginx': }
 
   File {
-    ensure        => 'present',
-    owner         => 'www-data',
-    group         => 'www-data',
-    mode          => '0640',
+    ensure => 'present',
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0640',
   }
 
   file { '/etc/nginx/sites-available/default':
-    require       => Package['nginx'],
-    content       => epp('base/nginx/default.epp'),
-    notify        => Service['nginx'], # Restart nginx server if being updated
+    require => Package['nginx'],
+    content => epp('base/nginx/default.epp'),
+    notify  => Service['nginx'], # Restart nginx server if being updated
   }
 
   file { '/etc/ssh/sshd_config':
-    require       => Package[ $ssh_packages ],
-    source        => ['puppet:///modules/base/ssh/sshd_config'],
-    notify        => Service['ssh'], # Restart ssh server if being updated
+    require => Package[$::ssh_packages],
+    source  => ['puppet:///modules/base/ssh/sshd_config'],
+    notify  => Service['ssh'], # Restart ssh server if being updated
   }
 
   service { 'nginx':
-    ensure        => running,
-    enable        => true,
+    ensure => running,
+    enable => true,
   }
 
   service { 'ssh':
-    ensure        => running,
-    enable        => true,
-    hasstatus     => true,
-    hasrestart    => true,
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
   }
 
 }
