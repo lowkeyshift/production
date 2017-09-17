f90699a68bde441fd22b65741a4a5609fc331139
Edwin <ecarra7979@gmail.com>
2017-09-17 00:18:57 -0400

    First push of demo code

---
+++ manifests/site.pp
@@ -0,0 +1,12 @@
+node default {
+  include base
+}
+
+node "agent-01.hsd1.ma.comcast.net" {
+  include base
+}
+
+#node "agent-02.hsd1.ma.comcast.net" {
+#  include second_demo
+#}
+
---
+++ modules/base/manifests/init.pp
@@ -0,0 +1,45 @@
+#Class: base
+#
+class base {
+
+  $base_packages = ['openssh-client', 'openssh-server']
+
+  Package {
+    ensure        => present,
+  }
+
+  package { $base_packages: }
+  package { 'nginx': }
+
+  File { 
+    ensure        => 'present',
+    owner         => 'root',
+    group         => 'root',
+    mode          => '0640',
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    require       => Package['nginx'],
+    content       => epp('base/nginx/default.epp'),
+    notify        => Service['nginx'], # Restart nginx server if being updated
+  }
+
+  file { '/etc/ssh/sshd_config':
+    require       => Package[ $ssh_packages ],
+    source        => ['puppet:///modules/base/ssh/sshd_config'],
+    notify        => Service['ssh'], # Restart ssh server if being updated
+  }
+
+  service { 'nginx':
+    ensure        => running,
+    enable        => true,
+  }
+
+  service { 'ssh':
+    ensure        => running,
+    enable        => true,
+    hasstatus     => true,
+    hasrestart    => true,
+  }
+
+}
