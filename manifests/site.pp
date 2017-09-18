862f412890a040e6058bd54a3fa7aff1c706612e
Edwin Carrasquillo <ecarra7979@gmail.com>
2017-09-18 02:19:12 -0400

    Deploying websites agent-02

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@ node 'agent-01.hsd1.ma.comcast.net' {
   include base, website02
 }
 
-#node 'agent-02.hsd1.ma.comcast.net' {
-#  include base
-#}
+node 'agent-02.hsd1.ma.comcast.net' {
+  include base website02
+}
