90753364ca9c478c2fa8d1fe5e5442cb26ba191a
Edwin Carrasquillo <ecarra7979@gmail.com>
2017-09-17 22:58:38 -0400

    updated website image and commented out agent-02

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,10 @@ node default {
   include base
 }
 
-node "agent-01.hsd1.ma.comcast.net" {
+node 'agent-01.hsd1.ma.comcast.net' {
   include base, website
 }
 
-#node "agent-02.hsd1.ma.comcast.net" {
-#  include website02
+#node 'agent-02.hsd1.ma.comcast.net' {
+#  include base
 #}
