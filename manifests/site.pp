8a9a481008665fa63ca8d1a0e06d546242ee0094
Edwin Carrasquillo <ecarra7979@gmail.com>
2017-09-18 02:57:02 -0400

    testing deployment

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@ node 'agent-01.hsd1.ma.comcast.net' {
   include base, website
 }
 
-#node 'agent-02.hsd1.ma.comcast.net' {
-#  include base, website02
-#}
+node 'agent-02.hsd1.ma.comcast.net' {
+  include base, website02
+}
